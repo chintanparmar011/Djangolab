bef46fdb058e3a90f3950aebdc83692ab029a878
chintan parmar <chintanparmar291206@gmail.com>
2026-01-29 22:14:54 +0530

    registration,sign in and sign out added - LAB 7 (#1)

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+   path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("", views.home, name="home"),
+    path("logout", views.logout_request, name="logout"),
+
 ]
